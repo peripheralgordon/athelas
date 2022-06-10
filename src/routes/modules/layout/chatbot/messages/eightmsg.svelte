ee79fe79166766b13a98db285f5cf5a0b4870b19
fancel30 <fancel30@posteitaliane.it>
2022-06-10 11:44:23 +0200

    add doctor icon

--- src/routes/modules/layout/chatbot/messages/eightmsg.svelte
+++ src/routes/modules/layout/chatbot/messages/eightmsg.svelte
@@ -12,7 +12,7 @@
 
 <div class="flex flex-col justify-between overflow-hidden bg-white pb-6">
     <div in:fly="{{ x: -1000, duration: 200 }}" class="mb-3 bg-blue-700  rounded-lg text-white w-fit">
-        <p class="p-2 pl-4 pr-4">Abbiamo finito {$username}! In base alle tue risposte ho alcuni consigli sul tuo stile di vita...</p>
+        <p class="p-2 pl-4 pr-4">Abbiamo finito {$username}! In base alle tue risposte ho valutato il tuo stile di vita...</p>
     </div>
 
     <button class={btnStyle} on:click={showResults}>mostra risultati</button>
